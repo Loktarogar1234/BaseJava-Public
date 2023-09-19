a4138a333f76d721c07a0dd83cbbd4653bb2c385
Loktarogar1234 <145042096+Loktarogar1234@users.noreply.github.com>
2023-09-20 00:52:32 +0300

    1

---
+++ Lesson_1/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        //todo сумма чисел в диапазоне 1 - 5
+        int sum1 = 1 + 2;
+        int sum2 = sum1 + 3;
+        int sum3 = sum2 + 4;
+        int result = sum3 + 5;
+        System.out.println(result);
+    }
+}
