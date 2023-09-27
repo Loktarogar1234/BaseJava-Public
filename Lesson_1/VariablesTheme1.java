f7acb27853bf0746e62d2581c42015a961729f95
Loktarogar1234 <145042096+Loktarogar1234@users.noreply.github.com>
2023-09-28 00:14:18 +0300

    sum

--- Lesson_1/VariablesTheme1.java
+++ Lesson_1/VariablesTheme1.java
@@ -1,5 +1,5 @@
 import java.lang.String;
-public class VariablesTheme {
+public class VariablesTheme1 {
     public static void main(String[] args) {
         
         System.out.println("Задание 1. Вывод характеристик компьютера");
