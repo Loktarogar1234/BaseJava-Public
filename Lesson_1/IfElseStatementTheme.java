c722f77b3751f0eab521a4d7da66d09a4551a49a
Loktarogar1234 <145042096+Loktarogar1234@users.noreply.github.com>
2023-10-15 02:12:31 +0300

    закончил 2 и 3 задание

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -2,9 +2,9 @@ public class IfElseStatementTheme  {
     public static void main(String[] args) {
         
         System.out.println("\nЗадание 1. Вывод характеристик компьютера");
-            int age = 21;
-            double height = 1.8;
-            boolean genderMan = true;
+        int age = 21;
+        double height = 1.8;
+        boolean genderMan = true;
 
         if(!genderMan){
             System.out.println("You are WOMAN");
@@ -31,24 +31,39 @@ public class IfElseStatementTheme  {
             System.out.println("First letter of your name is I");
         } else { System.out.println("First letter of your name is neither M nor I. You probably cheating!");
         }
+        System.out.println("\nЗадание 2. Поиск большего числа");
+        //заданию не противоречит, и запрета на рандом не было. интервал от 0 до 99
+        int variable1 = (int) (Math.random() * 100);
+        int variable2 = (int) (Math.random() * 100);
+        //для проверки на равенство. проверено, работает
+        //int variable1 = 5, variable2 = 5;
+        
+        if(variable1 > variable2){
+            System.out.println(variable1 + " больше, чем " + variable2);
+        } else if(variable1 < variable2){
+            System.out.println(variable2 + " больше, чем " + variable1);
+        } else {
+            System.out.println("оба числа равны (" + variable1 + " = " + variable2 + ")");
+        }
+        System.out.println("\nЗадание 3. Проверка числа");
+        //интервал от -100 до 100
+        int variable3 = (int) (Math.random()*(200+1)) - 100;
+       
+        if(variable3 == 0){
+            System.out.println("Число " + variable3 + " равно нулю");
+        } else if(variable3 > 0 && variable3 % 2 == 0){
+            System.out.println("Число " + variable3 + " положительное и чётное");
+        } else if(variable3 > 0 && variable3 % 2 != 0){
+            System.out.println("Число " + variable3 + " положительное и нечётное");
+        } else if(variable3 < 0 && variable3 % 2 == 0){
+            System.out.println("Число " + variable3 + " отрицательное и чётное");
+        } else {
+            System.out.println("Число " + variable3 + " отрицательное и нечётное");
+        }
+        
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+     
     
     }
 }
