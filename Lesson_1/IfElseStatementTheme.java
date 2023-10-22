6d2cbb852fc9cea28ddfcb93efeb8b42c613382d
Loktarogar1234 <evmixa@gmail.com>
2023-10-23 00:46:01 +0300

    исправления по замечаниям

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -1,86 +1,114 @@
 public class IfElseStatementTheme  {
     public static void main(String[] args) {
-        
+
         System.out.println("\nЗадание 1. Вывод характеристик компьютера");
         int age = 21;
         double height = 1.8;
         boolean genderMan = true;
 
-        if(!genderMan){
+        if(!genderMan) {
             System.out.println("Вы женщина");
         } else {
             System.out.println("Вы мужчина");
         }
     
-        if(age>18){
+        if(age > 18) {
             System.out.println("Вы взрослый");
         } else {
             System.out.println("Ты ребенок");
         }
 
-        if(height>1.7){
+        if(height > 1.7) {
             System.out.println("Вы высокий");
         } else {
             System.out.println("Вы низкого роста");
         }
 
         char firstNameLetter = "Eugene".charAt(0);
-        if(firstNameLetter == 'M'){
+        if(firstNameLetter == 'M') {
             System.out.println("Первая буква в вашем имени - M");
         } else if(firstNameLetter == 'I') {
             System.out.println("Первая буква в вашем имени - I");
-        } else { System.out.println("M и I не являются первыми буквами в вашем имени");
-        }
-        System.out.println("\nЗадание 2. Поиск большего числа");
-        //заданию не противоречит, и запрета на рандом не было. диапазон значений от 0 до 99
-        int variable1 = (int) (Math.random() * 100);
-        int variable2 = (int) (Math.random() * 100);
-        //для проверки на равенство. проверено, работает
-        //int variable1 = 5, variable2 = 5;
-        
-        if(variable1 > variable2){
-            System.out.println(variable1 + " больше, чем " + variable2);
-        } else if(variable1 < variable2){
-            System.out.println(variable2 + " больше, чем " + variable1);
         } else {
-            System.out.println("оба числа равны (" + variable1 + " = " + variable2 + ")");
+            System.out.println("M и I не являются первыми буквами в вашем имени Eugene");
         }
+
+        System.out.println("\nЗадание 2. Поиск большего числа");
+        //диапазон значений от 0 до 99
+        int num1 = (int) (Math.random() * 100);
+        int num2 = (int) (Math.random() * 100);
+        //для проверки на равенство. проверено, работает
+        //int num1 = 5, num2 = 5;
+        
+        if(num1 > num2) {
+            System.out.println(num1 + " больше, чем " + num2);
+        } else if(num1 < num2) {
+            System.out.println(num2 + " больше, чем " + num1);
+        } else {
+            System.out.println("оба числа равны (" + num1 + " = " + num2 + ")");
+        }
+
         System.out.println("\nЗадание 3. Проверка числа");
         //диапазон значений от -100 до 100
-        int variable3 = (int) (Math.random() * 201 - 100);
-       
-        if(variable3 == 0){
-            System.out.println("Число " + variable3 + " равно нулю");
-        } else if(variable3 > 0 && variable3 % 2 == 0){
-            System.out.println("Число " + variable3 + " положительное и чётное");
-        } else if(variable3 > 0 && variable3 % 2 != 0){
-            System.out.println("Число " + variable3 + " положительное и нечётное");
-        } else if(variable3 < 0 && variable3 % 2 == 0){
-            System.out.println("Число " + variable3 + " отрицательное и чётное");
+        int num3 = (int) (Math.random() * 201 - 100);
+
+        //первый вариант решения
+        if(num3 == 0) {
+            System.out.println("Число = " + num3);
+        } else if(num3 > 0 && num3 % 2 == 0) {
+            System.out.println("Число " + num3 + " положительное и чётное");
+        } else if(num3 > 0 && num3 % 2 != 0) {
+            System.out.println("Число " + num3 + " положительное и нечётное");
+        } else if(num3 < 0 && num3 % 2 == 0) {
+            System.out.println("Число " + num3 + " отрицательное и чётное");
         } else {
-            System.out.println("Число " + variable3 + " отрицательное и нечётное");
+            System.out.println("Число " + num3 + " отрицательное и нечётное");
         }
-        
+
+        //второй вариант решения (новый)
+        if (num3 % 2 == 0) {
+            if (num3 > 0) {
+                System.out.println("Число " + num3 + " положительное и чётное");
+            } else if (num3 <0) {
+                System.out.println("Число = " + num3);
+            }
+        } else {
+            if (num3 > 0) {
+                System.out.println("Число " + num3 + " положительное и нечётное");
+            } else {
+                System.out.println("Число " + num3 + " отрицательное и нечётное");
+            }
+        }
+
         System.out.println("\nЗадание 4. Поиск одинаковых цифр в числах");
         //диапазон значений от 100 до 999
-        int variable4 = (int) (Math.random() * 900 + 100);
-        int variable5 = (int) (Math.random() * 900 + 100);
-        //int variable4 = 123;      //проверил, работает
-        //int variable5 = 223;
-        System.out.println("Исходные числа = " + variable4 + " " + variable5);
+        int num4 = (int) (Math.random() * 900 + 100);
+        int num5 = (int) (Math.random() * 900 + 100);
+        System.out.println("Исходные числа = " + num4 + " " + num5);
         boolean coincidences = false;
+        int digit1_1 = num4 % 10;
+        int digit1_2 = num5 % 10;
+        if (digit1_1 == digit1_2) {
+            coincidences = true;
+            System.out.println("Одинаковая цифра = " + digit1_1 + " в разряде № 1");
+        }
 
-        for (int i = 0; i < 3; i++) {
-            int digit1 = variable4 % 10;
-            int digit2 = variable5 % 10;
-            if (digit1 == digit2) {
-                coincidences = true;
-                System.out.println("Одинаковая цифра = " + digit1 + " в разряде № " + (i + 1));
-            }
-            variable4 /= 10;
-            variable5 /= 10;
-            if (!coincidences) 
-                System.out.println("Равных цифр в разрядах нет");
+        int digit2_1 = (num4 / 10) % 10;
+        int digit2_2 = (num5 / 10) % 10;
+        if (digit2_1 == digit2_2) {
+            coincidences = true;
+            System.out.println("Одинаковая цифра = " + digit2_1 + " в разряде № 2");
+        }
+
+        int digit3_1 = num4 / 100;
+        int digit3_2 = num5 / 100;
+        if (digit3_1 == digit3_2) {
+            coincidences = true;
+            System.out.println("Одинаковая цифра = " + digit3_1 + " в разряде № 3");
+        }
+
+        if (!coincidences) {
+            System.out.println("Равных цифр в разрядах нет");
         }
 
         System.out.println("\nЗадание 5. Определение символа по его коду");
@@ -99,31 +127,30 @@ public class IfElseStatementTheme  {
         }
 
         System.out.println("\nЗадание 6. Подсчет суммы вклада и начисленных банком %");
-        int depo = 301000;
-        System.out.println("ваш депозит " + depo + " руб.");
-        if(depo < 100000){
-            System.out.println("% начислено " + (depo / 100 * 5) + " руб. Итого капитализация "
-                + (depo /100 * 105) + "руб.");
-        } else if ((depo > 100000) | (depo < 300000)){
-            System.out.println("% начислено " + (depo /100 * 7) + " руб. Итого капитализация "
-                + (depo / 100 * 107) + "руб.");
+        int deposit = 301000; //депо - устоявшийся разговорный термин, имхо оправданно был использован
+        int depositsitInterest = 0;
+        if(deposit < 100000) {
+            depositsitInterest = deposit / 100 * 5;
+        } else if (deposit > 100000 | deposit < 300000) {
+            depositsitInterest = deposit /100 * 7;
         } else {
-            System.out.println("% начислено " + (depo /100 * 10) + " руб. Итого капитализация "
-                + (depo /100 * 110) + "руб.");
+            depositsitInterest = deposit /100 * 10;
         }
+        System.out.println("Ваш депозит " + deposit + " руб. Начислено % " + depositsitInterest + " руб. " +
+        "Итого капитализация " + (deposit /100 * 105) + " руб.");
 
         System.out.println("\nЗадание 7. Определение оценки по предметам");
         //два вечера ломал голову над реализацией именно такой логики
         double historyGrade = 59;
         double programmingGrade = 92;
 
-        int historyScore = getGrade(historyGrade);
-        int programmingScore = getGrade(programmingGrade);
+        int history = getGrade(historyGrade);
+        int programming = getGrade(programmingGrade);
 
-        System.out.println("история " + historyScore);
-        System.out.println("программирование " + programmingScore);
+        System.out.println("история " + history);
+        System.out.println("программирование " + programming);
 
-        double averageScore = (historyScore + programmingScore) / 2.0;
+        double averageScore = (history + programming) / 2.0;
         System.out.println("средний балл оценок по предметам " + averageScore);
 
         double averagePercentage = (historyGrade + programmingGrade) / 2.0;
@@ -131,14 +158,12 @@ public class IfElseStatementTheme  {
 
         System.out.println("\nЗадание 8. Расчет годовой прибыли");
         int monthlySales = 19000;
-        int monthlyRent = 5000;
+        int monthlyRent = 50000; // на убытке работает корректно
         int monthlyProductionCost = 9000;
         int ebitda = (monthlySales - monthlyRent - monthlyProductionCost) * 12;
 
         if (ebitda > 0) {
             System.out.println("EBITDA равна +" + ebitda + " рублей");
-        } else if (ebitda < 0) {
-            System.out.println("EBITDA равна -" + ebitda + " рублей");
         } else {
             System.out.println("EBITDA равна " + ebitda + " рублей");
         }
