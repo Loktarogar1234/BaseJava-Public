916ad7538aafc5d5477868f6e7169849e6f05524
Lizzy <evmixa@gmail.com>
2023-11-11 00:06:57 +0300

    Person

---
+++ Lesson_2/test.java
@@ -0,0 +1,13 @@
+package Lesson_2;
+
+public class test {
+/*
+    public static void main(String[] args) {
+        Person person = new Person("мужчина", "Иван", 175, 70, 30);
+        person.walk();
+        person.sit();
+        person.run();
+        person.talk();
+        person.learnJava();
+    }*/
+}
