1f60ac667d79dedeee960f60a95371b07cb405fb
Evgeny <evmixa@gmail.com>
2023-12-07 00:47:19 +0300

    07.12.2023

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,13 +1,53 @@
 package Lesson_2;
 
 public class Wolf {
-    String gender;
-    String name;
-    double weight;
-    int age;
-    String color;
 
-    //методы класса
+    private String gender;
+    private String name;
+    private double weight;
+    private int age;
+    private String color;
+
+    public String getGender() {
+        return gender;
+    }
+
+    void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
+
     public void walk() {
         System.out.println("Идет");
     }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -4,25 +4,27 @@ public class WolfTest {
     public static void main(String[] args) {
         Wolf myWolf = new Wolf();
 
-        //присваивание значений
-        myWolf.gender = "Самец";
-        myWolf.name = "Альфа";
-        myWolf.weight = 45.5;
-        myWolf.age = 6;
-        myWolf.color = "Серый";
+        //присваивание значений через сеттеры
+        myWolf.setGender("Самец");
+        myWolf.setName("Альфа");
+        myWolf.setWeight(45.5);
+        myWolf.setAge(6);
+        myWolf.setColor("Серый");
 
-        //вывод значений
-        System.out.println("Пол: " + myWolf.gender);
-        System.out.println("Кличка: " + myWolf.name);
-        System.out.println("Вес: " + myWolf.weight);
-        System.out.println("Возраст: " + myWolf.age);
-        System.out.println("Окрас: " + myWolf.color);
+        //вывод значений через геттер
+        System.out.println("Пол: " + myWolf.getGender());
+        System.out.println("Кличка: " + myWolf.getName());
+        System.out.println("Вес: " + myWolf.getGender());
+        System.out.println("Возраст: " + myWolf.getAge());
+        System.out.println("Окрас: " + myWolf.getColor());
 
+        /*
         //вызов методов
         myWolf.walk();
         myWolf.sit();
         myWolf.run();
         myWolf.howl();
         myWolf.hunt();
+        */
     }
 }
